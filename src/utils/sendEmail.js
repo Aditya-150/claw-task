9484eeb6d41a7371e2bbf3b4125c5286afe55c24
Aditya Prasad Mohanty <adityaprasadmohanty150@gmail.com>
2024-07-16 17:45:42 +0530

    fix: pkg change

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -1,18 +1,30 @@
-// src/utils/sendEmail.js
 import Plunk from "@plunk/node";
 import { render } from "@react-email/render";
-import { createCanvas } from "canvas";
+import html2canvas from "html2canvas";
 import JsBarcode from "jsbarcode";
 import EventEmail from "../components/email/EventEmail";
+import { createElement } from "react";
+import ReactDOMServer from "react-dom/server";
 
-const generateBarcode = (text) => {
-  const canvas = createCanvas();
+const generateBarcodeImage = async (text) => {
+  const canvas = document.createElement("canvas");
   JsBarcode(canvas, text, {
     format: "CODE128",
     background: "transparent",
     lineColor: "black",
   });
-  return canvas.toDataURL("image/png");
+
+  const barcodeContainer = document.createElement("div");
+  barcodeContainer.appendChild(canvas);
+  document.body.appendChild(barcodeContainer);
+
+  const barcodeImage = await html2canvas(barcodeContainer).then((canvas) => {
+    const dataURL = canvas.toDataURL("image/png");
+    document.body.removeChild(barcodeContainer);
+    return dataURL;
+  });
+
+  return barcodeImage;
 };
 
 const sendEmail = async ({
@@ -24,18 +36,18 @@ const sendEmail = async ({
   name,
   to,
 }) => {
-  const barcodeImage = generateBarcode(`${time} ${date}`);
+  const barcodeImage = await generateBarcodeImage(`${time} ${date}`);
 
   const emailHtml = render(
-    <EventEmail
-      horse={horse}
-      date={date}
-      time={time}
-      location={location}
-      eventLink={eventLink}
-      name={name}
-      barcodeImage={barcodeImage}
-    />
+    createElement(EventEmail, {
+      horse,
+      date,
+      time,
+      location,
+      eventLink,
+      name,
+      barcodeImage,
+    })
   );
 
   const plunk = new Plunk(
